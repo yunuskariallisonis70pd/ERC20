a2cbfc1391c72b3f06c6f0ac7541a06de08a6a50
yunuskariallisonis70pd <yunuskariallisonis70pd@hotmail.com>
2024-09-18 11:21:25 +0000

    Initialised

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("ll","ll"){} 
+    constructor()ERC20("jj","jj"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
